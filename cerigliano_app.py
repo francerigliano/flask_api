ba6c7ca983aa52fb05689f506f8e00a82a5c192e
francerigliano <56144408+francerigliano@users.noreply.github.com>
2024-11-12 01:16:50 -0300

    Update cerigliano_app.py

--- cerigliano_app.py
+++ cerigliano_app.py
@@ -3,12 +3,12 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #Now I create the instance of Flask application
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dna_verification.db' #Then I make the necessary SQL Alchemy configurations, I name the local sqlite database as dna_verification.db
-#db = SQLAlchemy(app) #Afterwards I initialize SQLAlchemy with the Flask app for database management.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dna_verification.db' #Then I make the necessary SQL Alchemy configurations, I name the local sqlite database as dna_verification.db
+db = SQLAlchemy(app) #Afterwards I initialize SQLAlchemy with the Flask app for database management.
 
-#class DNAResult(db.Model): #Class that creates the database model
-    #id = db.Column(db.Integer, primary_key=True) #ID column as an auto-incrementing integer, its also the primary key
-    #is_mutant = db.Column(db.Boolean, nullable=False) #Boolean column that indicates if its Mutant or not
+class DNAResult(db.Model): #Class that creates the database model
+    id = db.Column(db.Integer, primary_key=True) #ID column as an auto-incrementing integer, its also the primary key
+    is_mutant = db.Column(db.Boolean, nullable=False) #Boolean column that indicates if its Mutant or not
 
 #Now I construct the function that can check if DNA sequence is mutant or not
 def isMutant(dna_sequence):
@@ -73,9 +73,9 @@ def check_dna():  #First I define the function definition for handling POST requ
         _, is_mutant = isMutant(dna)  #Afterwards I run the `isMutant` function to check if the DNA sequence is mutant.
         
         #The I create a new `DNAResult` object with the result of the DNA check.
-        #result = DNAResult(is_mutant=is_mutant)
-        #db.session.add(result)  #This line is to add the new result object to the database session.
-        #db.session.commit()  #This other line is to commit the session to save the new record to the database.
+        result = DNAResult(is_mutant=is_mutant)
+        db.session.add(result)  #This line is to add the new result object to the database session.
+        db.session.commit()  #This other line is to commit the session to save the new record to the database.
         
         if is_mutant:  # Check if the result indicates mutant DNA.
             return jsonify({"message": "Mutant DNA detected"}), 200  #Return a JSON response with status 200-OK.
@@ -85,20 +85,20 @@ def check_dna():  #First I define the function definition for handling POST requ
         return jsonify({"error": str(e)}), 402  #Return a JSON response with status 402 and the error message.
 
 #@app.route('/stats', methods=['GET'])  #Next I set up the '/stats' endpoint to accept GET requests.
-#def get_stats():  #This other function definition is for handling GET requests at the '/stats' endpoint.
-    #total_mutants = DNAResult.query.filter_by(is_mutant=True).count()  #First I count the number of mutant DNA results in the database.
-    #total_humans = DNAResult.query.filter_by(is_mutant=False).count()  #Second I count the number of human DNA results in the database.
+def get_stats():  #This other function definition is for handling GET requests at the '/stats' endpoint.
+    total_mutants = DNAResult.query.filter_by(is_mutant=True).count()  #First I count the number of mutant DNA results in the database.
+    total_humans = DNAResult.query.filter_by(is_mutant=False).count()  #Second I count the number of human DNA results in the database.
     
     #I define the variable 'ratio' to calculate the ratio of mutants to humans, avoiding division by zero.
-    #ratio = total_mutants / total_humans if total_humans > 0 else 'undefined'
+    ratio = total_mutants / total_humans if total_humans > 0 else 'undefined'
 
     # Create a dictionary with the stats data.
-    #stats = {
-        #"count_mutant_dna": total_mutants,  #Number of mutant DNA results.
-        #"count_human_dna": total_humans,  #Number of human DNA results.
-        #"ratio": ratio  #The ratio of mutant to human DNA results.
-    #}
-    #return jsonify(stats)  #Return the stats as a JSON response.
+    stats = {
+        "count_mutant_dna": total_mutants,  #Number of mutant DNA results.
+        "count_human_dna": total_humans,  #Number of human DNA results.
+        "ratio": ratio  #The ratio of mutant to human DNA results.
+    }
+    return jsonify(stats)  #Return the stats as a JSON response.
 
 if __name__ == '__main__':  #This ensures the script runs only if it's the main module.
     with app.app_context():  #Set up the application context for database operations.
